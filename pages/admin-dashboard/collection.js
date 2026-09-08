d8ec1e69b138bf572a677258e76079a168aa06dd
MAXIMAX ALWAYS MAXIMIZE <louismaxauto600@gmail.com>
2026-09-08 07:25:56 -0400

    Add Firebase collections structure in collection.js
    Define constants for Firebase collection names used in the application.

---
+++ pages/admin-dashboard/collection.js
@@ -0,0 +1,28 @@
+/*****************************************************************
+ FIREBASE COLLECTION STRUCTURE
+******************************************************************/
+
+export const FIREBASE_COLLECTIONS = {
+
+  member_records: "member_records",
+
+  warnings: "warnings",
+
+  disciplinary_cases: "disciplinary_cases",
+
+  committee_votes: "committee_votes",
+
+  appeals: "appeals",
+
+  suspensions: "suspensions",
+
+  audit_logs: "audit_logs",
+
+  governance: "governance",
+
+  administrative_team: "administrative_team",
+
+  disciplinary_committee: "disciplinary_committee",
+
+  advisory_council: "advisory_council"
+};
